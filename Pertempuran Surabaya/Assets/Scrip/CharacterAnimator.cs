88bcc62301e0739beea8737a1e5f9946c28298ee
Angga Andika Putra <45745570+anggaandika@users.noreply.github.com>
2020-08-03 14:52:55 +0700

    upload

--- Pertempuran Surabaya/Assets/Scrip/CharacterAnimator.cs
+++ Pertempuran Surabaya/Assets/Scrip/CharacterAnimator.cs
@@ -5,17 +5,28 @@ using UnityEngine.AI;
 
 public class CharacterAnimator : MonoBehaviour
 {
+    public AnimationClip replacebleAttackAnim;
+    public AnimationClip[] defaultAttackAnimSet;
+    protected AnimationClip[] currentAttackAnimSet;
+
     const float locomationAnimationSmoothTime = .1f;
 
     NavMeshAgent agent;
     protected Animator animator;
     protected CharacterCombat combat;
+    protected AnimatorOverrideController animatorOverride;
     // Start is called before the first frame update
     protected virtual void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponentInChildren<Animator>();
         combat = GetComponent<CharacterCombat>();
+
+        animatorOverride = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = animatorOverride;
+
+        currentAttackAnimSet = defaultAttackAnimSet;
+        combat.OnAttack += OnAttack;
     }
 
     // Update is called once per frame
@@ -25,5 +36,12 @@ public class CharacterAnimator : MonoBehaviour
         animator.SetFloat("speedPrecent", speedPrecent, locomationAnimationSmoothTime, Time.deltaTime);
 
         animator.SetBool("inCombat",  combat.InCombat);
+    }
+
+    protected virtual void OnAttack()
+    {
+        animator.SetTrigger("attack");
+        int indexAttack = Random.Range(0, currentAttackAnimSet.Length);
+        animatorOverride[replacebleAttackAnim.name] = currentAttackAnimSet[indexAttack];
     }
 }
--- Pertempuran Surabaya/Assets/Scrip/PlayerAnimatior.cs
+++ Pertempuran Surabaya/Assets/Scrip/PlayerAnimatior.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PlayerAnimatior : CharacterAnimator
 {
+    public WeaponAnimations[] weaponAnimations;
+    Dictionary<Equipment, AnimationClip[]> weaponeAnimationDict;
     protected override void Start()
     {
         base.Start();
         EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+
+        weaponeAnimationDict = new Dictionary<Equipment, AnimationClip[]>();
+        foreach (WeaponAnimations a in weaponAnimations)
+        {
+            weaponeAnimationDict.Add(a.weapon, a.clips);
+        }
     }
 
     void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
@@ -15,9 +23,20 @@ public class PlayerAnimatior : CharacterAnimator
         if (newItem != null && newItem.equipSlot.Equals(EquipmentSlot.Rifel) || newItem.equipSlot.Equals(EquipmentSlot.Male) || newItem.equipSlot.Equals(EquipmentSlot.Smg))
         {
             animator.SetLayerWeight(1, 1);
+            if (weaponeAnimationDict.ContainsKey(newItem))
+            {
+                currentAttackAnimSet = weaponeAnimationDict[newItem];
+            }
         } else
         {
             animator.SetLayerWeight(1, 0);
+            currentAttackAnimSet = defaultAttackAnimSet;
         }
+    }
+    [System.Serializable]
+    public struct WeaponAnimations
+    {
+        public Equipment weapon;
+        public AnimationClip[] clips;
     }
 }
