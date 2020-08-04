3b8e59148ac1f477900507349e32e90c8f7e3de8
Angga Andika Putra <45745570+anggaandika@users.noreply.github.com>
2020-08-04 14:05:14 +0700

    Perbaikan
    gerak palyer dan animasi

---
+++ Pertempuran Surabaya/Assets/Scrip/MenuControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+   public void ButtonStart()
+   {
+   		SceneManager.LoadScene(1);
+   }
+   public void ButtonCredit()
+   {
+   		SceneManager.LoadScene(2);
+   }
+   public void ButtonQuit()
+   {
+   		Application.Quit();
+   }
+}
